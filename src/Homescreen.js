48dab48215d76276bdaf3e6a74e994b0623af861
Adam Harris <aharris88@users.noreply.github.com>
2016-10-05 17:21:47 -0600

    Fix BEM in Homescreen

--- src/Homescreen.js
+++ src/Homescreen.js
@@ -8,8 +8,8 @@ const Homescreen = ({date, isMilitaryTime, messageNotifications, missedCallNotif
 
   return (
     <div className="Home">
-      <div className="Home__datetime">
-        <div className="Home__datetime__time">
+      <div className="Home__Datetime">
+        <div className="Home__Time">
           <FormattedTime
             value={date}
             hour='numeric'
@@ -17,14 +17,14 @@ const Homescreen = ({date, isMilitaryTime, messageNotifications, missedCallNotif
             hour12={!isMilitaryTime}
           />
         </div>
-        <div className="Home__datetime__date">
+        <div className="Home__Date">
           <div className="Home__datetime__date__weekday">
             <FormattedDate
               value={date}
               weekday='short'
             />
           </div>
-          <div className="Home__datetime__date__dayMonth">
+          <div>
             <FormattedDate
               value={date}
               day='2-digit'
@@ -36,7 +36,7 @@ const Homescreen = ({date, isMilitaryTime, messageNotifications, missedCallNotif
           </div>
         </div>
       </div>
-      <div className="Home__notifications">
+      <div className="Home__Notifications">
       {messageNotificationMessage}
       {missedCallNotificationMessage}
       </div>
