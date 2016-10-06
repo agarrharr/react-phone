d5456cd55865e34b61569b101d7136cd8c6e340f
Adam Harris <aharris88@users.noreply.github.com>
2016-10-06 17:10:41 -0600

    Update Statusbar to use BEM correctly

--- src/Statusbar.js
+++ src/Statusbar.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './Statusbar.css';
 
 const Statusbar = ({isLocked, volumeLevel, batteryLevel, isBluetoothOn, carrier }) => {
-  const bluetooth = isBluetoothOn ? <div className="Statusbar__Icon Statusbar--Bluetooth"></div> : null;
-  const lock = isLocked ? <div className="Statusbar__Icon Statusbar--Lock"></div>: null;
-  const volume = volumeLevel === 0 ? <div className="Statusbar__Icon Statusbar--Vibrate"></div> : null;
-  const battery = <div className="Statusbar__Icon Statusbar--Battery"></div>;
+  const bluetooth = isBluetoothOn ? <div className="Statusbar__Icon Statusbar__Icon--bluetooth"></div> : null;
+  const lock = isLocked ? <div className="Statusbar__Icon Statusbar__Icon--lock"></div>: null;
+  const volume = volumeLevel === 0 ? <div className="Statusbar__Icon Statusbar__Icon--vibrate"></div> : null;
+  const battery = <div className="Statusbar__Icon Statusbar__Icon--battery"></div>;
 
   return (
     <div className="Statusbar">
       <div className="Statusbar__Left">
-        <div className="Statusbar__Icon Statusbar--Signal"></div>
+        <div className="Statusbar__Icon Statusbar__Icon--signal"></div>
         <div className="Statusbar__Carrier">{carrier}</div>
       </div>
       <div className="Statusbar__Right">
